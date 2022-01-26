b1f6c32c88eb245c110e616f17af845c59dc2d31
FrancoAguirre644 <francoaguirre644@gmail.com>
2022-01-26 19:36:07 -0300

    FE - Reply comments.

---
+++ client/src/components/comments/AvatarReply.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { IUser } from '../../interfaces/IUser';
+
+interface IProps {
+    user: IUser;
+    reply_user?: IUser
+}
+
+const AvatarReply: React.FC<IProps> = ({ user, reply_user }) => {
+    return (
+        <div className="avatar_reply">
+            <img src={user.avatar} alt="avatar" />
+
+            <div className="ms-1">
+                <small className='reply_text'>
+                    <Link to={`/profile/${reply_user?._id}`}
+                        style={{ textDecoration: 'none' }}>
+                        {user.name}
+                    </Link>
+                </small>
+            </div>
+        </div>
+    )
+};
+
+export default AvatarReply;
